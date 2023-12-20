94bdbc2da836d85920ae06bf2113a57d462c6c8f
Hamnahs <140153260+Hamnahs@users.noreply.github.com>
2023-12-20 15:33:54 +0500

    K

--- myApp.js
+++ myApp.js
@@ -1,42 +1,39 @@
 let express = require('express');
 let app = express();
 
-// Define a root-level logger middleware
-app.use(function middleware(req, res, next) {
+
+const logger = (req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
-    console.log("I'm a middleware...");
     next();
+};
+
+app.get("/", logger, (req, res) => {
+    res.sendFile('/views/index.html');
+});
+app.get("/json", logger, (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase")
+        res.json({
+            message: "Hello json".toUpperCase()
+        });
+    else
+        res.json({
+            message: "Hello json"
+        });
 });
 
-// Define your routes
-app.get('/json', (req, res) => {
-    res.json({ message: 'Hello, world!' });
+
+
+
+
+app.get("/json", (req, res) => {
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        message = message.toUpperCase();
+    }
+    res.json({
+        message: message
+    });
+
 });
-
-// Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-// app.get("/", (req, res) => {
-//     res.json({"message": "Hello json"})
-// });
-
-// app.get("/json", (req, res) => {
-//         let response;
-//         if (process.env.VAR_NAME === "allCaps") {
-//             response = "Hello World".toUpperCase();
-//         } else {
-//             response = "Hello World";
-//         }
-//         res.json({message: response})
-//     }
-// )
-
-
-// app should not serve files from any other folders except from /public directory
-// app.use("/public", express.static(__dirname + "/public"))
-
 
 module.exports = app;
