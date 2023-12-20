4a1689a452196e5c8e42473d12205707a11b792c
Hamnahs <140153260+Hamnahs@users.noreply.github.com>
2023-12-20 14:34:39 +0500

    update

--- myApp.js
+++ myApp.js
@@ -7,17 +7,27 @@ app.get("/", (req, res) => {
     res.json({"message": "Hello json"})
 });
 
-app.get("/json", (req, res) => {
-    let response;
-    if (process.env.VAR_NAME === "allCaps") {
-        response = "Hello World".toUpperCase();
-    } else {
-        response = "Hello World";
-    }
-        res.json({message:response})
-    }
-)
+// app.get("/json", (req, res) => {
+//         let response;
+//         if (process.env.VAR_NAME === "allCaps") {
+//             response = "Hello World".toUpperCase();
+//         } else {
+//             response = "Hello World";
+//         }
+//         res.json({message: response})
+//     }
+// )
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+};
+
+app.use(logger);
+
+app.get('/json', (req, res) => {
+    res.json({message: 'Hello, worldyyyyyyyyyyyyyy!'});
+});
 
 // app should not serve files from any other folders except from /public directory
 // app.use("/public", express.static(__dirname + "/public"))
